1d462405bca829c0dd7cdfd76971ecc295f4dffe
evrenay <evrenay16@gmail.com>
2017-09-29 20:20:51 +0300

    denemee

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'evren' });
+  res.render('index', { title: 'evrendf' });
 });
 
 module.exports = router;
